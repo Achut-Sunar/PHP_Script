dba769d2e4efda197b18eb0e145a10be081fa9db
Achut Sunar <achutsunar121@gmail.com>
2026-04-19 16:01:59 +0545

    fix index

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 	<br>
 	<?php
 		// Get the application environment parameters from the Parameter Store.
-		//include ('get-parameters.php');
+		include ('get-parameters.php');
 
 		// Display the server metadata information if the showServerInfo parameter is true.
 	
